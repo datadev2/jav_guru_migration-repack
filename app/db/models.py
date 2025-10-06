8f9d97d99b92a88f890a9dc71362f54877f8ce48
Mikhail Onufrienko <ghast13@yandex.ru>
2025-10-06 11:57:42 +0200

    Add Video.thumbnail_s3_url attr & VideoCSV.poster_url attr

--- app/db/models.py
+++ app/db/models.py
@@ -75,6 +75,7 @@ class Video(Document):
     site: str = Field(default="unknown")
 
     thumbnail_url: HttpUrl | None = None
+    thumbnail_s3_url: HttpUrl | None = None
 
     categories: list[Link[Category]] = Field(default_factory=list)
     tags: list[Link[Tag]] = Field(default_factory=list)
@@ -128,6 +129,8 @@ class VideoCSV(BaseModel):
     categories: list
     tags: list
     s3_path: str
+    studio: str
+    poster_url: str
 
     @model_validator(mode="after")
     def validator(self):
