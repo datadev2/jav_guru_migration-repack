dbefa555578962ed45f2d7db32d33833f056f76a
Samangelof <samangelofw@gmail.com>
2025-11-27 16:31:24 +0300

    fix: fixed the order of loading configs

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,12 +7,13 @@ import pytest_asyncio
 from beanie import init_beanie
 from motor.motor_asyncio import AsyncIOMotorClient
 
+os.environ.setdefault("KVS_FEED_ENDPOINT", "http://localhost")
+os.environ.setdefault("KVS_FEED_PASSWORD", "TESTPASS")
+
 from app.config import config
 from app.db.models import Category, Model, Studio, Tag, Video
 
 
-os.environ.setdefault("KVS_FEED_ENDPOINT", "http://localhost")
-os.environ.setdefault("KVS_FEED_PASSWORD", "TESTPASS")
 
 
 @pytest.fixture(scope="module")
